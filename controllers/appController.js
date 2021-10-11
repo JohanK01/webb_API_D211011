62244713e455007a90433c325a61ae71b24833e2
JohanK <klausenjohan23@gmail.com>
2021-10-11 15:30:30 +0200

    writes out all ids in console webb

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,5 +1,5 @@
 exports.app = (req, res) => {
-
+    
     res.sendfile('public/html/app.html');
 
 };
--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,31 @@
 'use strict'
-
 var Airtable = require('airtable');
-var base = new Airtable({
-    apiKey: 'key0KdSnwdsofQz4R'
-}).base('appyrWJTLrVDtTCF6');
+var base = new Airtable({apiKey: 'key0KdSnwdsofQz4R'}).base('appyrWJTLrVDtTCF6');
 
 base('Design projects').select({
-    // Selecting the first 3 records in All projects:
-    maxRecords: 3,
+    
+    
     view: "All projects"
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
-    records.forEach(function (record) {
-        console.log('Retrieved', record.get('Name'));
+    records.forEach(function(record) {
+        console.log('Retrieved', record.id);
     });
 
     // To fetch the next page of records, call `fetchNextPage`.
     // If there are more records, `page` will get called again.
     // If there are no more records, `done` will get called.
-    fetchNextPage();
+    
 
 }, function done(err) {
-    if (err) {
-        console.error(err);
-        return;
-    }
+    if (err) { console.error(err); return; }
 });
+
+
+
+    // let MyList = document.createElement('ul');
+    // const listItem = document.createElement('li');
+    // listItem.textContent = record.id;
+    // MyList.appendChild(listItem);
+    // document.getElementById('Ex1').appendChild(MyList);
