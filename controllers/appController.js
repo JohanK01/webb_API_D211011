f1e2b7f6dc4179784b6d172f9343891f2d520b11
JohanK <klausenjohan23@gmail.com>
2021-10-11 14:21:10 +0200

    some json info writes in console in app.html

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,17 +1,5 @@
 exports.app = (req, res) => {
 
-    var Airtable = require('airtable');
-    var base = new Airtable({
-        apiKey: 'key0KdSnwdsofQz4R'
-    }).base('appyrWJTLrVDtTCF6');
-
-    base('Design projects').find('recyGmGkDzfVNeIQM', function (err, record) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('Retrieved', record.fields.Name);
-    });
     res.sendfile('public/html/app.html');
 
 };
