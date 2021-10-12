52e237f33323433d0bcae9376601930125e882e4
JohanK <klausenjohan23@gmail.com>
2021-10-12 14:29:23 +0200

    writes a list of id in document

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 'use strict'
 
 const req = new XMLHttpRequest;
-req.open('get', data);
+req.open('get', '/data');
+req.responseType = 'json';
+req.send();
 
+req.onload = () => {
+    const data = req.response;
+    console.log(data);
+
+    let MyList = document.createElement('ul');
+    MyList.id = 'List';
+    document.getElementById('Ex1').appendChild(MyList);
+    data.forEach(element => {
+        console.log(element.ID);
+        const ListItem = document.createElement('li');
+        ListItem.textContent = element.ID;
+        document.getElementById('List').appendChild(ListItem);
+    });
+    
+};
